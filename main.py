b5db2cf6cee5da57aa398a502422d469e5bb94a2
MaheshKumarMK <maheshmkvb92@gmail.com>
2022-10-19 13:09:57 +0530

    modified main.py file

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ def helloworld():
     return "Hello World"
 
 if __name__=='__main__':
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app",host='127.0.0.1', port=5000, reload=False, )
